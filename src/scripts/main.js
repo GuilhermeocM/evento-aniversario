e9643ee983f9486763bec2871d01e41d1f5c2c0c
Guilherme Mascarenhas <guilhermeoc.m@gmail.com>
2025-03-15 22:19:20 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+const dataDoEvento = new Date("Mar 07, 2026 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const haorasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${haorasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+}, 1000);
